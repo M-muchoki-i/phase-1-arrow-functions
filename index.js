dfe39afdcc1990d062d8801c572cbf8a3446daa6
michelle <michellemuchoki8@gmail.com>
2025-03-13 22:53:44 +0300

    arrows

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide() {
+    let x = 2000;
+    let y = 100;
+    return x / y;
+}
+const square = (x) => x * x;
+console.log(square(6));
+
+const add = (a, b) => a + b;
+console.log(add(4,5))
